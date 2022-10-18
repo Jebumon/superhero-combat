453927238a91d43fc800ef66a8f5f18250f5ff2f
Jebumon <jebumon@gmail.com>
2022-10-18 11:38:08 +0100

    Created API_handler Class and Modified Repository

---
+++ SuperheroAPI/Repository/SuperheroCombatRepository.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using SuperheroAPI.Models;
+namespace SuperheroAPI.Repository
+{
+    public class SuperheroCombatRepository
+    {
+        List<Contestant> contestantsList = new List<Contestant>();
+
+
+        public List<Contestant> GetContestants(string[] names)
+        {
+            return contestantsList;
+        }
+        public List<Contestant> GetPowerstats(string name)
+        {
+            return contestantsList;
+        }
+    }
+}

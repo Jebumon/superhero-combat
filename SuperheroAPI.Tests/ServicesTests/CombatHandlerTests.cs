d89497fdc4debeba66d724ada7fec4952d06ce9c
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-10-18 11:04:55 +0100

    Added CombatHandler logic

--- SuperheroAPI.Tests/ServicesTests/CombatHandlerTests.cs
+++ SuperheroAPI.Tests/ServicesTests/CombatHandlerTests.cs
@@ -1,6 +1,21 @@
-﻿namespace SuperheroAPI.Tests.ServicesTests
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace SuperheroAPI.Tests.ServicesTests
 {
-    internal class CombatHandlerTests
+    public class CombatHandlerTests
     {
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Check_BattleField_Values_Are_Valid()
+        {
+            
+        }
+
     }
 }

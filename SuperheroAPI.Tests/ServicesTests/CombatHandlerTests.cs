4e1c18e0022867ec9153a59a7b01397529d22aa8
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-10-19 10:31:07 +0100

    Added logic for close call and some more.. solid win, no chance and tie.Added testcases for the same

--- SuperheroAPI.Tests/ServicesTests/CombatHandlerTests.cs
+++ SuperheroAPI.Tests/ServicesTests/CombatHandlerTests.cs
@@ -1,21 +1,68 @@
 ﻿using Microsoft.AspNetCore.Components.Routing;
+using SuperheroAPI.Models;
+using SuperheroAPI.Services;
+using FluentAssertions;
 
 namespace SuperheroAPI.Tests.ServicesTests
 {
     public class CombatHandlerTests
     {
-
+        private Battlefield _battlefield;
+        private CombatHandler _combatHandler;
+        private CombatResult _combatResult;
         [SetUp]
         public void Setup()
         {
-           
+            _combatHandler = new();
         }
 
         [Test]
-        public void Check_BattleField_Values_Are_Valid()
+        public void Calculate_Correct_WinMargin_As_SolidWin_And_Winner()
         {
-            
+            List<Contestant> ContestantList = new List<Contestant>();
+            ContestantList.Add(new Contestant("Batgirl", 88, 11, 33, 40, 34, 90));
+            ContestantList.Add(new Contestant("Falcon", 38, 13, 50, 28, 22, 64));
+            _battlefield = new Battlefield("Volcano", 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f);
+            _combatResult = _combatHandler.DoCombat(ContestantList, _battlefield);
+            _combatResult.WinMargin.Should().Be(WinMargin.SolidWin);
+            _combatResult.Winner.Should().Be("Batgirl");
         }
 
+        [Test]
+        public void Calculate_Correct_WinMargin_As_NoChance_And_Winner()
+        {
+            List<Contestant> ContestantList = new List<Contestant>();
+            ContestantList.Add(new Contestant("A-Bomb", 8, 3, 10, 8, 2, 4));
+            ContestantList.Add(new Contestant("Batgirl", 88, 11, 33, 40, 34, 90));
+            _battlefield = new Battlefield("Volcano", 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f);
+            _combatResult = _combatHandler.DoCombat(ContestantList, _battlefield);
+            _combatResult.WinMargin.Should().Be(WinMargin.NoChance);
+            _combatResult.Winner.Should().Be("Batgirl");
+        }
+
+        [Test]
+        public void Calculate_Correct_WinMargin_As_CloseCall_And_Winner()
+        {
+            List<Contestant> ContestantList = new List<Contestant>();
+            ContestantList.Add(new Contestant("A-Bomb", 88, 13, 10, 38, 32, 94));
+            ContestantList.Add(new Contestant("Batgirl", 88, 11, 33, 40, 34, 90));
+            _battlefield = new Battlefield("Volcano", 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f);
+            _combatResult = _combatHandler.DoCombat(ContestantList, _battlefield);
+            _combatResult.WinMargin.Should().Be(WinMargin.CloseCall);
+            _combatResult.Winner.Should().Be("Batgirl");
+        }
+
+        [Test]
+        public void Calculate_Correct_WinMargin_As_Tie_And_Winner()
+        {
+            List<Contestant> ContestantList = new List<Contestant>();
+            ContestantList.Add(new Contestant("Batgirl", 88, 11, 33, 40, 34, 90));
+            ContestantList.Add(new Contestant("A-Bomb", 88, 11, 33, 40, 34, 90));
+            _battlefield = new Battlefield("Volcano", 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f);
+            _combatResult = _combatHandler.DoCombat(ContestantList, _battlefield);
+            _combatResult.WinMargin.Should().Be(WinMargin.Tie);
+            _combatResult.Winner.Should().Be("Both are winners");
+        }
     }
+
 }

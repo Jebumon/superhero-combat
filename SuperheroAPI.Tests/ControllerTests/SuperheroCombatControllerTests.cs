ca0b6b19c11f0063ba3a70bb61ae0bd717072887
Jas <doggrell@gmail.com>
2022-10-19 09:58:10 +0100

    Controller test where getting powerstats with a valid name succeeds

--- SuperheroAPI.Tests/ControllerTests/SuperheroCombatControllerTests.cs
+++ SuperheroAPI.Tests/ControllerTests/SuperheroCombatControllerTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using SuperheroAPI.Controllers;
 using SuperheroAPI.Models;
 using SuperheroAPI.Services;
@@ -6,12 +8,12 @@ namespace SuperheroAPI.Tests.ControllerTests;
 public class SuperheroCombatControllerTests
 {
     private SuperheroCombatController _controller;
-    private Mock<SuperheroCombatService> _mockSuperheroCombatService;
+    private Mock<ISuperheroCombatService> _mockSuperheroCombatService;
 
     [SetUp]
     public void Setup()
     {
-        _mockSuperheroCombatService = new Mock<SuperheroCombatService>();
+        _mockSuperheroCombatService = new Mock<ISuperheroCombatService>();
         _controller = new SuperheroCombatController(_mockSuperheroCombatService.Object);
     }
 
@@ -19,9 +21,17 @@ public class SuperheroCombatControllerTests
     public void GetSuperheroPowerstats_Gets_Powerstats_With_Valid_Name()
     {
         // Arrange
-        //var testContestant = new Contestant("Jasmine", 100, 100, 100, 100, 100, 100);
-       // _mockSuperheroCombatService.Setup(s => s.GetSuperheroPowerstats(Jasmine)).Returns(testContestant);
-        Assert.Pass();
+        string testName = "Superman";
+        List<Contestant> testContestant = new List<Contestant>();
+        testContestant.Add(new Contestant(testName, 100, 100, 100, 100, 100, 100));
+        _mockSuperheroCombatService.Setup(s => s.GetPowerstats(testName)).Returns(testContestant);
+
+        // Act
+        var result = _controller.GetSuperheroPowerstats(testName);
+
+        // Assert
+        result.Should().BeOfType(typeof(ActionResult<IEnumerable<Contestant>>));
+        result.Value.Should().BeEquivalentTo(testContestant);
     }
 
     [Test]

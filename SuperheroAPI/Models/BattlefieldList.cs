98d5b107bed5e1f2b0c870c7c500947aef974b75
CesarShogun <cesar.echavarren@gmail.com>
2022-10-25 12:42:37 +0100

    FIne-tuned the Battlefields

--- SuperheroAPI/Models/BattlefieldList.cs
+++ SuperheroAPI/Models/BattlefieldList.cs
@@ -4,12 +4,12 @@
     {
         private static readonly List<Battlefield> battlefields = new()
         {
-            new Battlefield("Volcano", 0.2f, 1f, 0.2f, 0.2f, 0.2f, 0.2f),
-            new Battlefield("Area51", 0.2f, 0.2f, 1f, 0.2f, 0.2f, 0.2f),
-            new Battlefield("HighGPlanet", 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 1f),
-            new Battlefield("Multiverse", 0.2f, 0.2f, 0.2f, 1f, 0.2f, 0.2f),
-            new Battlefield("BankVault", 1f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f),
-            new Battlefield("OpenField", 0.2f, 0.2f, 0.2f, 0.2f, 1f, 0.2f)
+            new Battlefield("Volcano", 0.7f, 1f, 0.4f, 0.7f, 0.5f, 0.7f),
+            new Battlefield("Area51", 0.7f, 0.7f, 1f, 0.5f, 0.5f, 0.6f),
+            new Battlefield("HighGPlanet", 0.9f, 0.9f, 0.3f, 0.8f, 1f, 0.1f),
+            new Battlefield("Multiverse", 0.6f, 0.6f, 0.6f, 1f, 0.6f, 0.6f),
+            new Battlefield("BankVault", 1f, 0.9f, 0.1f, 0.9f, 0.9f, 0.2f),
+            new Battlefield("OpenField", 0.7f, 0.7f, 0.2f, 0.8f, 0.6f, 1f)
         };
 
         public static Battlefield GetBattlefield(string name)
